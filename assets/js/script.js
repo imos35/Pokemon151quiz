3441e84204fdb6e7ee7cac698b56de1a84792956
Willhelm Gustavsson <ycqezdcp@students.codeinstitute.net>
2024-11-07 22:56:14 +0000

    added functionalityctionality to type buttons and filtered unwanted poke types

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,40 @@ console.log('script loaded');
 let pokemonImage = document.getElementById('pokemon-image');
 let pokemonName = document.getElementById('pokemon-name');
 
-let pokemonType;
+let pokemonType = [];
+let selectedTypes = [];
 
 async function getPokemon() {
     try {
-
-        let randomPokemon = Math.floor(Math.random() * 151) + 1;
+        let randomPokemon = (Math.floor(Math.random() * 151) + 1);
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`);
         let data = await response.json();
 
         let name = data.name;
         let sprite = data.sprites.front_default;
-        let type = data.types[0].type.name;
+        let type = data.types.map(typeInfo => typeInfo.type.name);
+        console.log(type)
+
+        if (name.toLowerCase() === 'magneton' || name.toLowerCase() === 'magnemite') {
+            pokemonType = type.filter(type => type !== 'steel');
+        } 
+        else if(name.toLowerCase() === 'clefable' 
+        || name.toLowerCase() === 'clefairy') {
+            pokemonType = type.filter(type => type !== 'fairy');
+            pokemonType.push('normal') 
+        }
+        else if(name.toLowerCase() === 'wigglytuff') {
+            pokemonType = type.filter(type => type !== 'fairy');
+        }
+        else {
+            pokemonType = type;
+        }
 
         pokemonName.textContent = name;
         pokemonImage.src = sprite;
         pokemonImage.alt = name;
         pokemonImage.style.display = 'block';
+
 
     } catch (error) {
         console.log("Could not fetch Pokémon! Error:", error);
@@ -33,9 +50,38 @@ typeButtons.forEach(
 
 function handleClick(event){
     let clickedButton = event.currentTarget;
-    let pokemonType = clickedButton.getAttribute('data-type');
+    let buttonType = clickedButton.getAttribute('data-type');
 
-    console.log(`You clicked on: ${pokemonType}`);
+    typeButtons.forEach(button => button.classList.remove('pressed'));
+    clickedButton.classList.add('pressed');
+    setTimeout(() => {
+        clickedButton.classList.remove('pressed');
+    }, 100);
+
+    if(!selectedTypes.includes(buttonType)){
+        selectedTypes.push(buttonType)
+    }
+
+    if(pokemonType.length === selectedTypes.length){
+        let isCorrect = selectedTypes.every(type => pokemonType.includes(type));
+
+    if(isCorrect){
+        console.log('correct!')
+        selectedTypes = []
+        setTimeout(() => {
+            getPokemon();
+        }, 300);
+
+    } 
+    else {
+        console.log('wrong!')
+        selectedTypes = []
+    };
+}
+ 
+    document.getElementById('next-question').addEventListener('click', () => {getPokemon()});
+
+    console.log(`You clicked on: ${buttonType}`);
 }
 
 
